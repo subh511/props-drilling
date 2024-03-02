7b28009244d84276d9a0196dc9a17aca6cb27ae9
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-02 16:01:02 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 
-
+import { useState } from "react";
 function App() {
 
+   const [count,setCount] = useState(0);
+
   return (
-    <>
-    </>
+    <div>
+    <Count count={count}/> 
+    <Button count={count} setCount={setCount}/>
+    </div>
   )
 }
 
+function Count({count}){
+  return(
+    <div>
+    {count}
+    </div>
+  )
+}
+
+function Button({count,setCount}){
+  return(
+    <div>
+    <button onClick={()=> setCount(count+1)}>+</button>
+    <button onClick={()=> setCount(count-1)}>-</button>
+    </div>
+  )
+}
 export default App
