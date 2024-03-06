67131d781520a2df319f26b5283436279f1bd3be
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-06 20:03:36 +0530

    adding selector for optimization

--- src/Components/store/atoms/Count.jsx
+++ src/Components/store/atoms/Count.jsx
@@ -1,7 +1,20 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
   key: "countAtom",
   default: 0,
 });
  
+
+export const evenSelector = selector({
+  key:"evenSelector",  
+  // get: ({get})=>{
+  //   const count = get(countAtom);
+  //   return count % 2;
+  // }
+  //or we derive like the second one also
+  get: (props)=>{
+    const count = props.get(countAtom);
+    return count % 2;
+  }
+})
